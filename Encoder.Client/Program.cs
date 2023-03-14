97272b044c12ec414639dd168820a9b86c3f4282
Maksim Makaranka <maksim.makaranka@gmail.com>
2023-03-14 17:51:08 +0100

    feature: initial encoding client writing data to db

--- Encoder.Client/Program.cs
+++ Encoder.Client/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace Encoder.Client
 {
     class Program
     {
+        private static readonly string queryString = "INSERT INTO Message (Id, EncryptedValue) VALUES (@Id, @EncryptedValue)";
+        private static readonly string connectionString = "Server=(localdb)\\mssqllocaldb;Database=DoubleEncodingSystem;Trusted_Connection=True;MultipleActiveResultSets=true;";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while(true)
+            {
+                Console.WriteLine("Hello! Please, enter the message you want to proceed.");
+                var message = Console.ReadLine();
+                if (string.IsNullOrEmpty(message))
+                    break;
+                var id = Guid.NewGuid();
+
+                using SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@Id", id);
+                command.Parameters.AddWithValue("@EncryptedValue", message);
+                connection.Open();
+                var rowsAffected = command.ExecuteNonQuery();
+                if (rowsAffected != 1)
+                {
+                    Console.WriteLine("Unexpected database error occurred!");
+                    break;
+                }
+            }
         }
     }
 }

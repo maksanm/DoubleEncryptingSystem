9190587ce7cafecdda550ea50c5b980dcd3d7832
Maksim Makaranka <maksim.makaranka@gmail.com>
2023-03-14 19:18:37 +0100

    minor

--- Encoder.Client/Services/MessageDatabaseFacade.cs
+++ Encoder.Client/Services/MessageDatabaseFacade.cs
@@ -6,16 +6,17 @@ namespace Encoder.Client.Services
 {
     internal class MessageDatabaseFacade : IMessageDatabaseFacade
     {
-        private static readonly string insertMessageQueryStrung = "INSERT INTO Message (Id, EncryptedValue) VALUES (@Id, @EncryptedValue)";
+        private static readonly string insertMessageQueryString = "INSERT INTO Message (Id, EncryptedValue) VALUES (@Id, @EncryptedValue)";
         private static readonly string connectionString = "Server=(localdb)\\mssqllocaldb;Database=DoubleEncryptingSystem;Trusted_Connection=True;MultipleActiveResultSets=true;";
 
         public bool InsertMessage(string id, string encryptedValue)
         {
             using SqlConnection connection = new SqlConnection(connectionString);
-            SqlCommand command = new SqlCommand(insertMessageQueryStrung, connection);
+            SqlCommand command = new SqlCommand(insertMessageQueryString, connection);
             command.Parameters.AddWithValue("@Id", id);
             command.Parameters.AddWithValue("@EncryptedValue", encryptedValue);
             connection.Open();
+
             try
             {
                 var rowsAffected = command.ExecuteNonQuery();
@@ -30,7 +31,6 @@ namespace Encoder.Client.Services
                 Console.WriteLine("SQL query execution unexpected error! Error message: " + ex.Message);
                 return false;
             }
-
             return true;
         }
     }
